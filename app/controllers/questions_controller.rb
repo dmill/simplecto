27905a19e98afb0f6ed952bc985c6903df84fd44
Doug Mill <doug@empiri.co>
2016-07-14 14:07:25 -0400

    customizing devise user registration + question actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,25 @@
 class QuestionsController < ApplicationController
+
   def index
     @questions = current_user.questions
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.create(question_params)
+
+    render action: "show"
+  end
+
+  def show
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :question_text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
 
-  resources :users do
-    resources :questions
-  end
+  devise_for :users, controllers: { registrations: "users/registrations" }
+
+  resources :questions
 end
