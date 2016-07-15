d9b260ebd4dfc4c99aaf9004036c41e09fcf7861
Doug Mill <doug@empiri.co>
2016-07-15 14:55:32 -0400

    user validations and authentication on questions resources

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
   has_many :questions
+
+  validates_presence_of :email, :first_name, :last_name
 end
