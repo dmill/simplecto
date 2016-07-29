5db5c5a43e2acede12283730a43b137251f40a54
Andrew Wong <arwong09@gmail.com>
2016-07-29 16:43:37 -0400

    give a default for user photo_url

--- db/migrate/20160729193748_add_photo_url_to_users.rb
+++ db/migrate/20160729193748_add_photo_url_to_users.rb
@@ -1,5 +1,5 @@
 class AddPhotoUrlToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :photo_url, :string
+    add_column :users, :photo_url, :string, default: ""
   end
 end
